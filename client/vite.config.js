11cf94312346225aba0cdef5ce2652e092483a3f
Pnaruka054 <Pnaruka054@gmail.com>
2024-10-17 15:52:44 +0530

    vgdfgdfgdfgdf

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://link-shortner-server.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
+  build: {
+    outDir: 'dist',
   },
 });
