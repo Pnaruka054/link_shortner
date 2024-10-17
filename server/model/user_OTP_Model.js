ac460d2e17bff9871b52343e3620f1352506416f
Pnaruka054 <Pnaruka054@gmail.com>
2024-10-17 14:12:21 +0530

    sdlfsd

--- server/model/user_OTP_Model.js
+++ server/model/user_OTP_Model.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const OTP_Schema = new mongoose.Schema({
     user_id: { type: String, require: true },
     gmail: { type: String, require: true },
-    otp: { type: Number, require: true }
+    otp: { type: Number, require: true },
+    createdAt: { type: Date, default: Date.now, expires: '10m' }
 })
 
 const otp_data = mongoose.model('otp_Data', OTP_Schema)
